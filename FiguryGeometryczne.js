226a7e3e72e39ca7da31dab11f1774755042aced
Magda <magdap.265@gmail.com>
2019-06-28 14:17:11 +0200

    dopisanie kodu tworzącego tabelę oraz jej uzupełnianie danymi wprowadzanymi przez użytkownika w inputach

--- FiguryGeometryczne.js
+++ FiguryGeometryczne.js
@@ -14,19 +14,19 @@ class FiguraGeometryczna {
 
     WyswietlWlasnosci (){
         return {
-            nazwaFigury = this.nazwa,
-            iloscBokowFigury = this.iloscBokow
+            nazwaFigury: this.nazwa,
+            iloscBokow: this.iloscBokow
         }
     }
 }
 
 class Trojkat extends FiguraGeometryczna{
-    constructor (nazwa, iloscBokow, a, h, pole) {
+    constructor (nazwa, iloscBokow, a, h) {
         super(nazwa, iloscBokow);
-        this.wzorNaPole = "a*h*1/2";
         this.podstawa = a;
         this.wysokosc = h;
-        this.pole = pole;
+        this.wzorNaPole = "a*h*1/2";
+        this.pole = a*h*1/2;
     }
 
     static pole (a,h){
@@ -39,24 +39,24 @@ class Trojkat extends FiguraGeometryczna{
 
     WyswietlWlasnosci (){
         return {
-            nazwaFigury = this.nazwa,
-            iloscBokow = this.iloscBokow,
-            wzorNaPole = this.wzorNaPole,
-            podstawa = this.podstawa,
-            wysokosc = this.wysokosc,
-            pole = poleTrojkata()
+            nazwaFigury: this.nazwa,
+            iloscBokow: this.iloscBokow,
+            podstawa: this.podstawa,
+            wysokosc: this.wysokosc,
+            wzorNaPole: this.wzorNaPole,
+            pole: this.pole
         }
     }
 
 }
 
 class Prostokat extends FiguraGeometryczna{
-    constructor (nazwa, iloscBokow, a, b, pole){
+    constructor (nazwa, iloscBokow, a, b){
         super (nazwa, iloscBokow);
-        this.wzorNaPole = "a*b";
         this.bok1 = a;
         this.bok2 = b;
-        this.pole = pole;
+        this.wzorNaPole = "a*b";
+        this.pole = a*b;
     }
 
     static pole (a, b){
@@ -69,12 +69,71 @@ class Prostokat extends FiguraGeometryczna{
 
     WyswietlWlasnosci (){
         return {
-            nazwaFigury = this.nazwa,
-            iloscBokow = this.iloscBokow,
-            wzorNaPole = this.wzorNaPole,
-            bok1 = this.bok1,
-            bok2 = this.bok2,
-            pole = this.poleProstokata()
+            nazwaFigury: this.nazwa,
+            iloscBokow: this.iloscBokow,
+            bok1: this.bok1,
+            bok2: this.bok2,
+            wzorNaPole: this.wzorNaPole,
+            pole: this.pole
         }
     }
+}
+
+// Trójkąt testowy
+
+let trojkatTest = new Trojkat ("trójkąt testowy", 3, 4, 5);
+console.log(trojkatTest);
+
+// Prostokąt testowy
+
+let prostokatTest = new Prostokat ("prostokąt testowy", 4, 4, 5);
+console.log(prostokatTest);
+
+
+
+// funkcja pobierająca dane z przeglądarki i tworząda z nich w tabeli bazę figór
+
+function dodajTrojkat(){
+    let nazwaTrojkata = document.getElementById("nazwaTrojkata").value;
+    let podstawaTrojkata = document.getElementById("podstawa").value;
+    let wysokoscTrojkata = document.getElementById("wysokosc").value;
+
+    let nowyTrojkat = new Trojkat (nazwaTrojkata, 3, podstawaTrojkata, wysokoscTrojkata);
+    
+    let wlasnosciTrojkata = nowyTrojkat.WyswietlWlasnosci();
+    
+
+    // tworzenie tabeli na trójkąty
+   
+    let tabelaNaTrojkaty = document.getElementById("tabelaNaTrojkaty"); 
+
+    let nowyWiersz = tabelaNaTrojkaty.insertRow(-1);
+    let nazwaT = nowyWiersz.insertCell(0);
+    let iloscBokowT = nowyWiersz.insertCell(1);
+    let podstawaT = nowyWiersz.insertCell(2);
+    let wysokoscT = nowyWiersz.insertCell(3);
+    let wzorNaPoleT = nowyWiersz.insertCell(4);
+    let poleT = nowyWiersz.insertCell(5);
+
+    // wypełnianie tabeli
+    nazwaT.innerHTML = wlasnosciTrojkata.nazwaFigury;
+    iloscBokowT.innerHTML = wlasnosciTrojkata.iloscBokow;
+    podstawaT.innerHTML = wlasnosciTrojkata.podstawa;
+    wysokoscT.innerHTML = wlasnosciTrojkata.wysokosc;
+    wzorNaPoleT.innerHTML = wlasnosciTrojkata.wzorNaPole;
+    poleT. innerHTML = wlasnosciTrojkata.pole;
+        
+
+
+
+        // wyświetlanie danych potrzebnych do tabeli - test
+        // document.getElementById("tabelaNaTrojkaty").innerHTML = [nowyTrojkat.WyswietlWlasnosci().nazwaFigury,
+        //                                                 nowyTrojkat.WyswietlWlasnosci().iloscBokow,
+        //                                                 nowyTrojkat.WyswietlWlasnosci().podstawa,
+        //                                                 nowyTrojkat.WyswietlWlasnosci().wysokosc,
+        //                                                 nowyTrojkat.WyswietlWlasnosci().wzorNaPole,
+        //                                                 nowyTrojkat.WyswietlWlasnosci().pole];
+
+    
+    
 }
